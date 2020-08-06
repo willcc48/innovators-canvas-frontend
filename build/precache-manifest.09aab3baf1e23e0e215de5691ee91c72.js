d9ffb61da174114fec75cb19ab18639662299ce4
unknown <william.chase.carlson@gmail.com>
2020-08-05 21:35:21 -0400

    second commit

---
+++ build/precache-manifest.09aab3baf1e23e0e215de5691ee91c72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e58c372d04c7b7d059ccfd68c3dc5be0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c56e968eb7305ae5a907",
+    "url": "/static/css/2.b90d77b3.chunk.css"
+  },
+  {
+    "revision": "32f6528d8a10c9aeaef1",
+    "url": "/static/css/main.8c2094da.chunk.css"
+  },
+  {
+    "revision": "c56e968eb7305ae5a907",
+    "url": "/static/js/2.cafccba0.chunk.js"
+  },
+  {
+    "revision": "9b24e414a92a0db59d09cb19d285c913",
+    "url": "/static/js/2.cafccba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32f6528d8a10c9aeaef1",
+    "url": "/static/js/main.7439f0c5.chunk.js"
+  },
+  {
+    "revision": "c918743e5cafc3e6bf59",
+    "url": "/static/js/runtime-main.942529d8.js"
+  }
+]);
